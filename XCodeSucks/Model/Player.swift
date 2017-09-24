b12b1027d2026dae5802f3d39c5f68c403092811
danielodorizzi <danielsreef@me.com>
2017-09-23 23:50:46 -0600

    More code

---
+++ XCodeSucks/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  XCodeSucks
+//
+//  Created by Daniel Odorizzi on 9/23/17.
+//  Copyright © 2017 Daniel Odorizzi. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+    
+}
